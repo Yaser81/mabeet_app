29772403045df423c355346240017155e0ee4506
Yaser Absah <Yaser81@users.noreply.github.com>
2025-08-26 19:10:17 +0300

    update app icons

--- lib/features/splash/presentation/views/widgets/splash_body_view.dart
+++ lib/features/splash/presentation/views/widgets/splash_body_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:mabeet_app/home_screen.dart';
+import 'package:page_transition/page_transition.dart';
+
+import '../../../../../core/constant.dart';
 
 class SplashBodyView extends StatelessWidget {
   const SplashBodyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedSplashScreen(
+      splash: Column(
+        children: [
+          Image.asset('assets/logo.png', height: 100),
+          const SizedBox(height: 10),
+          const Text(
+            AppTitle,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      nextScreen: const HomeScreen(),
+      splashIconSize: 150,
+      duration: 2500, // الوقت بالملي ثانية
+      splashTransition: SplashTransition.scaleTransition,
+      pageTransitionType: PageTransitionType.fade,
+      backgroundColor: Colors.white,
+    );
   }
 }
